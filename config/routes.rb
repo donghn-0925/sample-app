74dcbe605875f70bbd1f64a6946445d540be4b3c
dong-0925 <hoang.nhat.dong@sun-asterisk.com>
2019-07-16 15:57:38 +0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root "application#hello"
+  root "static_pages#home"
 
   resources :static_pages do
     collection do
-      get :help, :home, :about
+      get :help, :home, :about, :contact
     end
   end
+  get "/signup", to: "users#new"
 end
